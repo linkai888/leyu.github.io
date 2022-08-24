574808fbd6a43e4d6415d16d8d4ba42005b8e8c7
linkai888 <111964961+linkai888@users.noreply.github.com>
2022-08-24 17:46:36 +0800

    Add files via upload

---
+++ index_files/config.js
@@ -0,0 +1,28 @@
+/**
+QQ 联系方式修改地方 exp [1111,22222,33333]
+**/
+var qqArr = [854974544,854974544];
+//乐鱼 移动端 注册网址
+var leyuReg = function(){
+	 window.location.href = "http://www.353leyuvip.com";
+};
+//乐鱼 移动端 下载网址
+var leyuDown =function(){
+	window.location.href = "http://www.353leyuvip.com";
+};
+//爱游戏 移动端 注册网址
+var hthReg = function(){
+	window.location.href = "http://www.353leyuvip.com";
+};
+//爱游戏 移动端 下载网址
+var hthDown = function(){
+	window.location.href = "http://www.353leyuvip.com";
+};
+//乐鱼 电脑端 注册网址
+var leyuRegPc = function(){
+	window.location.href = "http://www.353leyuvip.com";
+};
+//爱游戏 电脑端 注册网址
+var hthRegPc = function(){
+	window.location.href = "http://www.353leyuvip.com";
+};
