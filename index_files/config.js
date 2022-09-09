cff8c59ea678ccb71c937313c8ec6681b03278c4
linkai888 <111964961+linkai888@users.noreply.github.com>
2022-09-10 03:43:19 +0800

    Update config.js

--- index_files/config.js
+++ index_files/config.js
@@ -4,25 +4,25 @@ QQ 联系方式修改地方 exp [1111,22222,33333]
 var qqArr = [854974544,854974544];
 //乐鱼 移动端 注册网址
 var leyuReg = function(){
-	 window.location.href = "http://www.353leyuvip.com";
+	 window.location.href = "https://www.8nh1j.com:6001/entry/register/?i_code=4985798";
 };
 //乐鱼 移动端 下载网址
 var leyuDown =function(){
-	window.location.href = "http://www.353leyuvip.com";
+	window.location.href = "https://www.8nh1j.com:6001/entry/register/?i_code=4985798";
 };
 //爱游戏 移动端 注册网址
 var hthReg = function(){
-	window.location.href = "http://www.353leyuvip.com";
+	window.location.href = "https://www.8nh1j.com:6001/entry/register/?i_code=4985798";
 };
 //爱游戏 移动端 下载网址
 var hthDown = function(){
-	window.location.href = "http://www.353leyuvip.com";
+	window.location.href = "https://www.8nh1j.com:6001/entry/register/?i_code=4985798";
 };
 //乐鱼 电脑端 注册网址
 var leyuRegPc = function(){
-	window.location.href = "http://www.353leyuvip.com";
+	window.location.href = "https://www.8nh1j.com:6001/entry/register/?i_code=4985798";
 };
 //爱游戏 电脑端 注册网址
 var hthRegPc = function(){
-	window.location.href = "http://www.353leyuvip.com";
+	window.location.href = "https://www.8nh1j.com:6001/entry/register/?i_code=4985798";
 };
